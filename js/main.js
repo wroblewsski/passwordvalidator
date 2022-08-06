de0240810bfe372c373cf7a60f6bb8606a244a13
Wojciech <wroblewskiw91@gmail.com>
2022-08-06 14:07:54 +0200

    styles

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const showMsg = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.textContent = 'super hasło';
+		p.style.color = 'lime';
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+	) {
+		p.textContent = 'dobre hasło';
+		p.style.color = 'gold';
+	} else {
+		p.textContent = 'masz słabe hasło';
+		p.style.color = 'tomato';
+	}
+};
+
+pass.addEventListener('keyup', showMsg);
