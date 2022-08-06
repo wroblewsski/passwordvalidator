1a1a4f52e421d364b90f58c2b26b9a99f6c2a227
Wojciech <wroblewskiw91@gmail.com>
2022-08-06 12:53:12 +0200

    test changes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
